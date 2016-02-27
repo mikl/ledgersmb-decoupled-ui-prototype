89bd0772b04786aefec8316e9e19b0bb1b8c7477
Mikkel Hoegh <mikkel@hoegh.org>
2016-02-27 01:16:29 +0100

    Initial commit
    Starting out with the React Redux Starter Kit:
    https://github.com/davezuko/react-redux-starter-kit

---
+++ README.md
@@ -0,0 +1,4 @@
+LedgerSMB decoupled UI prototype
+================================
+
+An idea of how a decoupled UI could work.
---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,14 @@
+/* @flow */
+import React from 'react'
+
+export class HomeView extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Ledgersmb Decoupled Ui Prototype</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeView
